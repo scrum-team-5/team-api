f3bde96e76992cf07868fe49e8bc09107b4f0606
cris-rr <59718617+cris-rr@users.noreply.github.com>
2023-05-31 21:29:37 -0500

    cris line

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express =require('express');
+const express = require('express');
 
 const port = 3000;
 
 const app = express();
 
-app.get('/', (req,res) => {res.send("Hello Browser")});
+app.get('/', (req, res) => {
+  res.send('Hello Browser');
+});
 
-app.get('/matias', (req,res) => {res.send("Hello Matias")});
+app.get('/matias', (req, res) => {
+  res.send('Hello Matias');
+});
 
-app.listen(port, ()=>console.log("Listening"));
+app.get('/cris', (req, res) => {
+  res.send('Hello Cristhians');
+});
+
+app.listen(port, () => console.log('Listening'));
